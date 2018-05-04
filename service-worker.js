01b3034cf76cf9ad171b235403e8f62904c1cfb7
Macintosh <agil@tlab.co.id>
2018-05-04 14:22:45 +0700

    rev

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,18 @@
-const cacheVersionName = 'staticAssets';
-
-const cacheFiles = [
-  '/',
-  '/index.html',
-  '/dist/style.css',
-  '/dist/images/notif.svg',
-  '/app.js',
-  'https://fonts.googleapis.com/css?family=Karla:400,700'
-];
-
-
+ 
 self.addEventListener('install', function(event) {
   console.log('SW Installed!')
 
   event.waitUntil(
-    caches.open(cacheVersionName)
+    caches.open('staticAssets')
       .then(function(cache) {
-        return cache.addAll(cacheFiles)
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/dist/style.css',
+          '/dist/images/notif.svg',
+          '/app.js',
+          'https://fonts.googleapis.com/css?family=Karla:400,700'
+        ])
       })
   )
 });
