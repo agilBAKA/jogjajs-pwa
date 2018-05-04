fb4716bfe829b4c5ff66b3f533dccdf922f6b824
Macintosh <agil@tlab.co.id>
2018-05-04 14:27:31 +0700

    rev

--- service-worker.js
+++ service-worker.js
@@ -22,15 +22,28 @@ self.addEventListener('activate', function() {
 });
 
 
-self.addEventListener('fetch', function(event) {
+addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
-      .then(function(res) {
-        if (res) {
-          return res;
+      .then(function(response) {
+        if (response) {
+          return response;     // if valid response is found in cache return it
         } else {
-          return fetch(event.request);
+          return fetch(event.request)     //fetch from internet
+            .then(function(res) {
+              return caches.open('staticAssets')
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());    //save the response for future
+                  return res;   // return the fetched data
+                })
+            })
+            .catch(function(err) {       // fallback mechanism
+              return caches.open('staticAssets')
+                .then(function(cache) {
+                  return cache.match('/offline.html');
+                });
+            });
         }
       })
   );
-});
+}); 
